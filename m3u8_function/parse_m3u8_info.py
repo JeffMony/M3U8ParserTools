211c914149befe25911266ed592ea83cd790c3ee
JeffMony <jeffmony@163.com>
2020-09-22 14:53:08 +0800

    更新m3u8功能

--- m3u8_function/parse_m3u8_info.py
+++ m3u8_function/parse_m3u8_info.py
@@ -12,6 +12,7 @@ TAG_STREAM_INF='#EXT-X-STREAM-INF'
 
 URL='https://cdn3.lajiao-bo.com/20200113/DGKlALM5/index.m3u8'
 
+### 返回最终的m3u8文件
 def parse_m3u8_info(url):
     val = urlparse.urlsplit(url)
     if (val.scheme != 'http') and (val.scheme != 'https'):
@@ -33,7 +34,7 @@ def parse_m3u8_info(url):
         result += get_final_url(url, line) + '\n'
     return result
 
-
+### 根据m3u8中的分片写法得到完整的url
 def get_final_url(url, line):
     val = urlparse.urlsplit(url)
     hostUrl = url[0:url.index(val.netloc)+len(val.netloc)]
@@ -55,5 +56,36 @@ def get_final_url(url, line):
         return line
     return baseUrl + line
 
+### 判断m3u8中是否存在#EXT-X-STREAM-INF
+def has_ext_stream(url):
+    val = urlparse.urlsplit(url)
+    if (val.scheme != 'http') and (val.scheme != 'https'):
+        return 'Error protocol'
+    request = requests.get(url, timeout=20)
+    for line in request.iter_lines():
+        if (line.startswith(TAG_PREFIX)):
+            if (line.startswith(TAG_STREAM_INF)):
+                return True
+    return False
+
+### 判断m3u8中是否存在#EXT-X-KEY
+def has_ext_key(url):
+    val = urlparse.urlsplit(url)
+    if (val.scheme != 'http') and (val.scheme != 'https'):
+        return 'Error protocol'
+    request = requests.get(url, timeout=20)
+    for line in request.iter_lines():
+        if (line.startswith(TAG_PREFIX)):
+            if (line.startswith(TAG_KEY)):
+                return True
+    return False
+
 result = parse_m3u8_info(URL)
-print(result)
+print('M3U8文件内容如下:\n' + result)
+
+hasExtStream = has_ext_stream(URL)
+print('是否存在多路流: ---> ' + str(hasExtStream))
+
+hasExtKey = has_ext_key(URL)
+print('是否存在key: ---> ' + str(hasExtKey))
+
